2dca38e3383d0fbaad2ec240c60a1527ee0c2836
dannyluxmusic-jpg <dannyluxmusic@gmail.com>
2026-05-16 16:08:24 -0500

    Update build_runway_geometry.py

--- build_runway_geometry.py
+++ build_runway_geometry.py
@@ -17,8 +17,31 @@ zf = zipfile.ZipFile(io.BytesIO(response.content))
 with zf.open("APT.txt") as f:
     lines = f.read().decode("latin-1", errors="ignore").splitlines()
 
-for line in lines:
-    if line.startswith("RWY") and "16/34" in line:
-        print(line)
+count = 0
 
-print("DONE")
+for line in lines:
+
+    if not line.startswith("RWY"):
+        continue
+
+    if "16/34" not in line:
+        continue
+
+    print("\n===================")
+
+    print("AIRPORT:", line[3:10].strip())
+
+    print("RUNWAY:", line[13:20].strip())
+
+    print("END1 LAT:", line[83:97].strip())
+    print("END1 LON:", line[98:113].strip())
+
+    print("END2 LAT:", line[286:300].strip())
+    print("END2 LON:", line[301:316].strip())
+
+    count += 1
+
+    if count >= 5:
+        break
+
+print("\nDONE")
